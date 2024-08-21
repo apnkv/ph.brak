b59c343cb4054ed42514c338aff94c0c90e35c9c
apnkv <alexpnkv@gmail.com>
2024-08-21 23:32:13 +0100

    initial PH research graphs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,117 @@
-import Image from "next/image";
+interface GraphProps {
+  heading: string;
+  description: string;
+  plotlyEmbedUrl: string;
+  height: number;
+}
+
+const Graph = (props: GraphProps) => {
+  const { heading, description, plotlyEmbedUrl, height } = props;
+
+  return (
+    <div
+      className="flex flex-col gap-4"
+    >
+      <h2>
+        {heading}
+      </h2>
+
+      <p>
+        {description}
+      </p>
+
+      <div
+        className="w-full items-center justify-between font-mono text-sm lg:flex z-10 h-full"
+      >
+        <iframe
+          id="igraph"
+          scrolling="no"
+          style={{border: "none"}}
+          seamless="seamless"
+          src={`${plotlyEmbedUrl}?autosize=true&link=false&modebar=false&theme=light`}
+          height={height}
+          width="100%"
+        />
+      </div>
+    </div>
+  )
+}
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <>
+      <main className="flex min-h-screen flex-col items-center p-6 gap-6">
+        <Graph
+          heading="Lorem1 ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec
+          purus non nunc posuere blandit. Donec ut neque nec nisl aliquet
+          ultricies. Integer sit amet mi nec purus tincidunt ultricies. Nam
+          posuere, purus nec tincidunt ultricies, elit ex fermentum eros, ac
+          iaculis turpis mi nec metus. Nullam nec purus non nunc posuere blandit.
+          Donec ut neque nec nisl aliquet ultricies. Integer sit amet mi nec purus
+          tincidunt ultricies. Nam posuere, purus nec tincidunt ultricies, elit ex
+          fermentum eros, ac iaculis turpis mi nec metus."
+          plotlyEmbedUrl="https://chart-studio.plotly.com/~hpnkv/1.embed"
+          height={720}
         />
-      </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <Graph
+          heading="Lorem2 ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec
+          purus non nunc posuere blandit. Donec ut neque nec nisl aliquet
+          ultricies. Integer sit amet mi nec purus tincidunt ultricies. Nam
+          posuere, purus nec tincidunt ultricies, elit ex fermentum eros, ac
+          iaculis turpis mi nec metus. Nullam nec purus non nunc posuere blandit.
+          Donec ut neque nec nisl aliquet ultricies. Integer sit amet mi nec purus
+          tincidunt ultricies. Nam posuere, purus nec tincidunt ultricies, elit ex
+          fermentum eros, ac iaculis turpis mi nec metus."
+          plotlyEmbedUrl="https://chart-studio.plotly.com/~hpnkv/8.embed"
+          height={720}
+        />
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        <Graph
+          heading="Lorem3 ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec
+          purus non nunc posuere blandit. Donec ut neque nec nisl aliquet
+          ultricies. Integer sit amet mi nec purus tincidunt ultricies. Nam
+          posuere, purus nec tincidunt ultricies, elit ex fermentum eros, ac
+          iaculis turpis mi nec metus. Nullam nec purus non nunc posuere blandit.
+          Donec ut neque nec nisl aliquet ultricies. Integer sit amet mi nec purus
+          tincidunt ultricies. Nam posuere, purus nec tincidunt ultricies, elit ex
+          fermentum eros, ac iaculis turpis mi nec metus."
+          plotlyEmbedUrl="https://chart-studio.plotly.com/~hpnkv/11.embed"
+          height={720}
+        />
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
+        <div className="flex flex-col gap-4">
+          <h2>Lorem4</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec
+            purus non nunc posuere blandit. Donec ut neque nec nisl aliquet
+            ultricies. Integer sit amet mi nec purus tincidunt ultricies. Nam
+            posuere, purus nec tincidunt ultricies, elit ex fermentum eros, ac
+            iaculis turpis mi nec metus. Nullam nec purus non nunc posuere blandit.
+            Donec ut neque nec nisl aliquet ultricies. Integer sit amet mi nec purus
+            tincidunt ultricies. Nam posuere, purus nec tincidunt ultricies, elit ex
+            fermentum eros, ac iaculis turpis mi nec metus.
           </p>
-        </a>
+          <iframe src='https://flo.uri.sh/visualisation/19074068/embed' title='Interactive or visual content'
+                  className='flourish-embed-iframe' frameBorder='0' scrolling='no' style={{width: '100%', height: 720}}
+                  sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'></iframe>
+          <div
+            style={{width: '100%', marginTop: '4px', textAlign: 'right'}}
+          ><a className='flourish-credit'
+               href='https://public.flourish.studio/visualisation/19074068/?utm_source=embed&utm_campaign=visualisation/19074068'
+               target='_top'
+               style={{textDecoration: 'none'}}><img
+            alt='Made with Flourish' src='https://public.flourish.studio/resources/made_with_flourish.svg'
+            style={{width: '105px', height: '16px', border: 'none', margin: '0'}}/> </a></div>
+        </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+
+      </main>
+    </>
+
+  )
+    ;
 }
